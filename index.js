6d1b1054f94798bdcfd1fd3ed9902a714946295c
Numanazad <namanajid54@gmail.com>
2023-05-30 17:16:56 +0530

    clean

--- index.js
+++ index.js
@@ -1,38 +1,33 @@
 const express = require('express');
 const axios = require("axios");
 const app = express();
-const port = 3000; // Choose a port number of your choice
+const port = 3000;
 
 app.use(express.json());
 
 // Handle the incoming webhook request
 app.post('/webhook', (req, res) => {
-  // Process the request payload and forward it to Capri
   const payload = req.body; // The payload received from GHL
-  console.log("payload", payload);
-  return false;
-  // Forward the transformed payload to Capri
+  
   forwardPayloadToCapri(payload)
     .then(() => {
-      res.sendStatus(200); // Respond with a success status
+      res.sendStatus(200);
     })
     .catch((error) => {
       console.error('Error forwarding payload to Capri:', error);
-      res.sendStatus(500); // Respond with an error status
+      res.sendStatus(500);
     });
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Gateway server is running on port ${port}`);
 });
 
-// Function to forward the payload to Capri
+// Function to forward the payload
 async function forwardPayloadToCapri(payload) {
   try {
-    const capriUrl = 'https://systems.capriai.us/gpt3'; // Replace with the actual Capri URL
+    const capriUrl = 'https://systems.capriai.us/gpt3'; 
 
-    // Make a request to Capri with the transformed payload
     await axios.post(capriUrl, payload);
 
     console.log('Payload forwarded to Capri successfully');
